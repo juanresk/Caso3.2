55c0e750496f081e1bd70321a76f669031f7ff01
juanresk <ja.restrepok@uniandes.edu.co>
2020-10-18 16:14:41 -0500

    comienzo

---
+++ src/Cliente/ClienteUDP.java
@@ -0,0 +1,36 @@
+package Cliente;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.util.Scanner;
+
+public class ClienteUDP {
+    private DatagramSocket udpSocket;
+    private InetAddress serverAddress;
+    private int port;
+    private Scanner scanner;
+    private ClienteUDP(String destinationAddr, int port) throws IOException {
+        this.serverAddress = InetAddress.getByName(destinationAddr);
+        this.port = port;
+        udpSocket = new DatagramSocket(this.port);
+        scanner = new Scanner(System.in);
+    }
+    public static void main(String[] args) throws NumberFormatException, IOException {        
+        ClienteUDP sender = new ClienteUDP("localhost", 7077);
+        System.out.println("-- Running UDP Client at " + InetAddress.getLocalHost() + " --");
+        sender.start();
+    }
+    private int start() throws IOException {
+        String in;
+        while (true) {
+            in = scanner.nextLine();
+            
+            DatagramPacket p = new DatagramPacket(
+                in.getBytes(), in.getBytes().length, serverAddress, port);
+            
+            this.udpSocket.send(p);                    
+        }
+    }
+}
---
+++ src/ServidorUDP/ServidorUDP.java
@@ -0,0 +1,38 @@
+package ServidorUDP;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+public class ServidorUDP {
+    private DatagramSocket udpSocket;
+    private int port;
+ 
+    public ServidorUDP(int port) throws SocketException, IOException {
+        this.port = port;
+        this.udpSocket = new DatagramSocket(this.port);
+    }
+    private void listen() throws Exception {
+        System.out.println("-- Running Server at " + InetAddress.getLocalHost() + "--");
+        String msg;
+        
+        while (true) {
+            
+            byte[] buf = new byte[256];
+            DatagramPacket packet = new DatagramPacket(buf, buf.length);
+            
+            // blocks until a packet is received
+            udpSocket.receive(packet);
+            msg = new String(packet.getData()).trim();
+            
+            System.out.println(
+                "Message from " + packet.getAddress().getHostAddress() + ": " + msg);
+        }
+    }
+    
+    public static void main(String[] args) throws Exception {
+        ServidorUDP client = new ServidorUDP(7077);
+        client.listen();
+    }
+}
