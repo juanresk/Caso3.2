df5fd0cd775f3a89ab3323f9baac047bcb9aba81
juanresk <ja.restrepok@uniandes.edu.co>
2020-10-26 23:26:09 -0500

    Protocolo terminado

--- py/cliente.py
+++ py/cliente.py
@@ -1,31 +1,72 @@
 # Laboratorio 3.2 INFRACOM
 # LUIS MIGUEL GOMEZ LONDONO, JUAN ANTONIO RESTREPO KUNZEL, JULIAN DAVID MENDOZA RUIZ
 
+
 import socket
-import select
+import logging
+import time
+import hashlib
 
-serverAddressPort = ("0.0.0.0", 20001)
-bufferSize = 1024
-timeout = 3
+# Se inicializa el log, se le dan estas características para que lo escriba como queremos
+logging.basicConfig(filename="resultados.log", level=logging.INFO,
+                    format='%(asctime)s %(levelname)-8s %(message)s',
+                    datefmt='%Y-%m-%d %H:%M:%S'
+                    )
+hashMensaje=hashlib.sha256()
 
-# Creamos el socket UDP para la parte del cliente
-UDPClientSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
-UDPClientSocket.bind(serverAddressPort)
 
+# la ip donde está corrriendo el servidor localhost = 127.0.0.1
+host = 'localhost'
+#puerto de conexión del socket. En este caso es el puerto donde corre el servidor.
+port=8080
+
+buffersize = 1024
+
+UDPClientSocket=socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+#Se conecta al servidor
+UDPClientSocket.connect((host, port))
+print("listo para recibir informacion")
+logging.info("CLIENTE listo para recibir la info")
+UDPClientSocket.sendto("listo".encode(),(host,port))
+#Se empieza a recibir informacion
+num_paquetes=0
+f = open("recibido.mp4", 'wb')
 while True:
-    data, addr = UDPClientSocket.recvfrom(bufferSize)
-    if data:
-        print("Filename: {}".format(data))
-        filename = data.strip()
-    with open(filename, 'wb') as f:
-        while True:
-            ready = select.select([UDPClientSocket], [], [], timeout)
-            if ready[0]:
-                data, addr = UDPClientSocket.recvfrom(bufferSize)
-                f.write(data)
-            else:
-                print("%s Finish!" % filename)
-                f.close()
-                break
+    data,adrr=UDPClientSocket.recvfrom(buffersize)
+    start_time=time.time()
+    f.write(data)
+    #De contener el encabezado enviado entonces entra en el if
+    if (data.__contains__(b"HASH")):
+        
+        print("LLEGÓ DICIEMBRE")
+        
+        i=data.find(b"HASH")
+        hashMensaje.update(data[:i])
+        hash_recibido = data[i+4:] #Se le suman 4 debido a que se emvió con HASH + mensaje esos 4 son del HASH
+        print("El hash recibido es ", hash_recibido)
+        print("El hash calculado es",hashMensaje.hexdigest())
+
+        #Se revisa si el hash cuadra con el recibido del servidor
+        if hashMensaje.hexdigest()== hash_recibido.decode():
+            print("El hash llegó bien")
+            logging.info("Hash está correcto")
+            print("El numero de paquetes recibidos es de ", num_paquetes)
+            break
+        else:
+            print("hash llegó maluco")
+            logging.info("Hash incorrecto")
+            print("El numero de paquetes recibidos es de ", num_paquetes)
+            break
+
+        #Si ya se terminó el mensaje entonces salga de ahí soldado
+        if not data:
+            break
+
+    hashMensaje.update(data)
+    num_paquetes+=1
+finalTime = time.time() - start_time
+logging.info("CLIENTE Tiempo del envio %s" % finalTime)
+logging.info("---------------------------------------------")
 
 UDPClientSocket.close()
--- py/servidor.py
+++ py/servidor.py
@@ -1,27 +1,74 @@
 # Laboratorio 3.2 INFRACOM
 # LUIS MIGUEL GOMEZ LONDONO, JUAN ANTONIO RESTREPO KUNZEL, JULIAN DAVID MENDOZA RUIZ
 
-# Imports ****IMPORTAR pip3 install tqdm
 import socket
 import time
-import sys
+from threading import Thread
+import hashlib
 
-localIP = "0.0.0.0"
-localPort = 20001
-bufferSize = 1024
+buffersize = 1024
 
-filename = sys.argv[1]
+def main():
+    #Arreglo con las conexiones que se unan al servidor
+    connections = []
+    #IP donde corre el servidor
+    host='localhost'
+    #Puerto donde corre el servidor
+    port=8080
+    #Configuracion del socket del servidor. Se usa datagram para UDP y enviar los archivos
+    serverSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    
 
-# Creamos el datagram socket
-UDPServerSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
-UDPServerSocket.sendto(filename.encode(), (localIP, localPort))
-print("Sending %s ..." % filename)
+    #Crea el servidor
+    serverSocket.bind((host,port))
 
-with open(filename, "rb") as f:
-    data = f.read(bufferSize)
-    while(data):
-        if(UDPServerSocket.sendto(data, (localIP, localPort))):
-            data = f.read(bufferSize)
-            time.sleep(0.02)
-    f.close()
-UDPServerSocket.close()
+    print("El servidor esta escuchando en el puerto %d" %port)
+    texto=int (input("\n Cual video quiere mandar?"
+                     "\n 1. Etee es sech"
+                     "\n 2. Drake y josh"
+                     "\n"))
+    if texto==1:
+        efe="esteessech.mp4"
+    else:
+        efe="actualidad.mp4"
+    global file
+    file=efe
+    print("Archivo seleccionado: ",file)
+    numerocon= int( input('\n A cuantos clientes quiere enviar el archivo?' ))
+
+    print("Esperando conexiones")
+    cont=0
+    while cont<numerocon:
+        #establish connection with client
+        data,adrr=serverSocket.recvfrom(buffersize)
+        print("Recibi conexion",cont)
+        #Crea el thread para manejar la conexion
+        thread=Thread(target= start_thread, args=(serverSocket,adrr))
+        #Se agregan a la lista de conecciones las que van llegando
+        connections.append(thread)
+        cont+=1
+    #Se recorre la lista de threads y se les da start con la tarea que tienen que hacer   
+    for i in connections:
+        i.start()
+
+
+def start_thread(s,adrr):
+    hashSha=hashlib.sha256()
+    num_paquetes=0
+    with open(file,'rb') as f:
+        while True:
+            data=f.read(buffersize)
+            if not data:
+                print ("termine de leer")
+                #en este punto es necesario enviar el hash al cliente
+                h=str(hashSha.hexdigest())
+                print("Hash a enviar: "+h)
+                #Se le envía con el encabezado HASH para poder encontrarlo después
+                s.sendto(("HASH"+h).encode(),adrr)
+                print("El numero de paquetes eviados es de ",num_paquetes)
+                break
+            hashSha.update(data)
+            s.sendto(data,adrr)
+            num_paquetes+=1
+        
+main()
